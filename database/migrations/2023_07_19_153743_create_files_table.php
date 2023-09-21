89dbfe8732bd1213fc121794a40a0d4d0ab5317b
hethmjal <hethmjal5@gmail.com>
2023-09-21 14:44:33 +0300

    last update

--- database/migrations/2023_07_19_153743_create_files_table.php
+++ database/migrations/2023_07_19_153743_create_files_table.php
@@ -23,9 +23,10 @@ return new class extends Migration
 
     /**
      * Reverse the migrations.
-     */
+    */
     public function down(): void
     {
         Schema::dropIfExists('files');
     }
+    
 };
